488caf4f0875e8ce562fd979398d847dadfe834d
Herrscherr0Z <71039974+Herrscherr0Z@users.noreply.github.com>
2022-10-23 18:29:20 -0500

    Add files via upload

---
+++ JOURNAL/dns-resolver.ps1
@@ -0,0 +1,11 @@
+﻿param ($N, $S)
+
+for ($x=1; $x -le 254; $x++) {
+    
+    $IPA = Resolve-DnsName -DnsOnly "$N.$x" -Server $S -ErrorAction Ignore | ForEach-Object {$_.Name}
+    $HN =  Resolve-DnsName -DnsOnly "$N.$x" -Server $S -ErrorAction Ignore | ForEach-Object {$_.NameHost}
+    $IPA | ForEach-Object {
+        $L = $IPA.split('.')[0]
+        echo "$N.$L $HN"
+    }
+}
