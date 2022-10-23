1af9d75ae072774a1f4bae06584a3a09182e555d
Herrscherr0Z <71039974+Herrscherr0Z@users.noreply.github.com>
2022-10-23 18:33:54 -0500

    Update dns-resolver.ps1

--- JOURNAL/dns-resolver.ps1
+++ JOURNAL/dns-resolver.ps1
@@ -1,5 +1,7 @@
-﻿param ($N, $S)
+# Making variables to enter network address ($N) with dns server IP ($S)
+param ($N, $S)
 
+# Making for loop to resolve dns names within the foster network
 for ($x=1; $x -le 254; $x++) {
     
     $IPA = Resolve-DnsName -DnsOnly "$N.$x" -Server $S -ErrorAction Ignore | ForEach-Object {$_.Name}
